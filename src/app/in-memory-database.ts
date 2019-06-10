9e7a70e4a24d21fdf6db8a4ae29c88f148293cf5
gusvagner <gustavovagner@hotmail.com>
2019-06-10 16:34:37 -0300

    Adding Entry stuff

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,5 +1,6 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 import { Category } from './pages/categories/shared/category.model';
+import { Entry } from './pages/entries/shared/entry.model';
 
 
 export class InMemoryDatabase implements InMemoryDbService {
@@ -11,7 +12,13 @@ export class InMemoryDatabase implements InMemoryDbService {
             { id: 3, name: "Lazer", description: "Cinema, parques, praia, etc." }
         ];
 
-        return { categories }
+        const entries: Entry[] = [
+            { id: 1, name: "Uber", categoryId: categories[0].id, category: categories[0], paid: true, date: "17/10/2018", amount:"15,00", type:"expense" } as Entry,
+            { id: 2, name: "Aluguel", categoryId: categories[1].id, category: categories[1], paid: true, date: "17/10/2018", amount:"800,00", type:"expense" } as Entry,
+            { id: 3, name: "Carro", categoryId: categories[2].id, category: categories[2], paid: false, date: "17/10/2018", amount:"10000,00", type:"expense" } as Entry
+        ]
+
+        return { categories, entries }
 
     };
 
